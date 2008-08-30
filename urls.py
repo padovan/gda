a9d9d4e57a2ddc3f6c3ea29b725b81ea8f6b181c
Gustavo S. Scalet <gut@las.ic.unicamp.br>
2008-08-30 18:11:47 +0000

    new urls.py that redirect the requests of the server to the new functions on the sad/views.py
    git-svn-id: https://sad-fenix.googlecode.com/svn/trunk@11 aa523ed0-0652-0410-bfd5-2b4be68e55b5

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from caco.sad.views import *
+from settings import TEMPLATE_DIRS
 
 urlpatterns = patterns('',
     # Example:
@@ -6,4 +8,10 @@ urlpatterns = patterns('',
 
     # Uncomment this for admin:
      (r'^admin/', include('django.contrib.admin.urls')),
+     (r'^$', selecionaDisciplina),
+     (r'^preenche/(?P<req_sigla>\w{5})/$', preencheDisciplina),
+     (r'^salva/(?P<req_sigla>\w{5})/', salvaDisciplina),
+     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+         'document_root': TEMPLATE_DIRS[1]
+         })
 )
