4c15600ae7f939a990b6b17977d4f2de75ce3897
Gustavo S. Scalet <gut@las.ic.unicamp.br>
2008-08-30 18:10:54 +0000

    added a template directory
    git-svn-id: https://sad-fenix.googlecode.com/svn/trunk@10 aa523ed0-0652-0410-bfd5-2b4be68e55b5

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for caco project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -70,6 +71,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.getcwd(),'templates'),
+    os.path.join(os.getcwd(),'templates','media'),
 )
 
 INSTALLED_APPS = (
