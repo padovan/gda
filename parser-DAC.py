02fbb29384afe82d29afa05ae5ce9285cfbab8c4
Gustavo F. Padovan <gustavo@las.ic.unicamp.br>
2008-11-02 05:40:58 +0000

    Improve parser-DAC - more modularized - get data from all institutes of UNICAMP if we want
    We still need to review the regex. They can be better.
    
    --pao
    
    
    
    git-svn-id: https://sad-fenix.googlecode.com/svn/trunk@31 aa523ed0-0652-0410-bfd5-2b4be68e55b5

--- parser-DAC.py
+++ parser-DAC.py
@@ -4,7 +4,7 @@
 import re
 import sys, os
 import mechanize
-import urllib2
+#import urllib2
 from time import sleep
 
 from caco.sad.models import *
@@ -22,6 +22,21 @@ DRE_PROF = 'Docente: (?P<docente>.*)\\r\\n'
 DRE_HOR_POS = '\<a href="(?P<pos_hor>[MD][0-9]{5,5}\.htm)"\>'
 DRE_HOR_POS_DETAIL = '<font size=-1>(?P<disc_id>[A-Z][A-Z ][0-9]{3,3})(?P<disc_nome>.*)   </font>'
 
+
+
+def iconv_file(file, name):
+
+    f = open(name + ".ascii", 'w')
+    f.write(file) 
+    f.close()
+    os.system("iconv -f iso8859-1 -t utf-8 " + name + ".ascii >" + \
+            name + ".utf8")
+    f = open(name + ".utf8", 'r')
+    r = f.read()
+    f.close()
+    os.remove(name + ".utf8")
+    os.remove(name + ".ascii")
+    return r
 
 
 def get_site(base, file):
@@ -52,7 +67,13 @@ def add_disciplina(ld , all):
     return all
 
 
-# primeira parte: Discubrir as disciplinas de um dado semestre
+# primeira parte: Descubrir as disciplinas de um dado semestre
+
+# Estamos descobrindo quais são as disciplinas de cada semestre através do site
+# da DAC. Lá a página para ver o horário da grad e diferente do da pós. Nao pós
+# existem várias modalidades de mestrado e doutorado enquanto na grad não. Por
+# isso duas funções aqui: get_disc_grad() and get_disc_pos().
+
 def get_disc_grad():
     
     getbase = BASE_SITE
@@ -60,6 +81,7 @@ def get_disc_grad():
     s_disc = get_site(getbase, getfile)
     
     d_all_disc = re.compile(DRE_ALL_DISC)
+    # gera lista de disciplinas
     l_disc = re.findall(d_all_disc, s_disc)
     r = add_disciplina(l_disc, [])
     return r
@@ -75,18 +97,22 @@ def get_disc_pos():
     for p_hor in pages:
         site = get_site(getbase, p_hor)
         d_all_disc = re.compile(DRE_HOR_POS_DETAIL)
+        # gera lista de disciplinas 
         l_disc = re.findall(d_all_disc, site)
         r = add_disciplina(l_disc, r)
     return r
 
 
-def get_matriculados(txtDisciplina):
+# dada uma disciplina, get_matriculados descobre quais as turmas existentes e
+# a partir desses dados descobre todos os dados sobre cada turma (alunos,
+# matriculados e professor)
+def get_matriculados(disc):
 
     mech = mechanize.Browser()
     mech.set_handle_robots(False)
 
     r = mech.open("http://www.daconline.unicamp.br/altmatr/menupublico.do")
-# encontra o token (hash dinamico para acesso)
+    # encontra o token (hash dinamico para acesso)
     site = r.read()
     dtoken = re.compile(DRE_TOKEN)
     m = re.search(dtoken, site)
@@ -96,14 +122,7 @@ def get_matriculados(txtDisciplina):
     token = m.group('token')
 
 # preenche o formulário pra pegar as turmas
-    res1 = mech.open("http://www.daconline.unicamp.br/altmatr/conspub_situacaovagaspordisciplina.do?org.apache.struts.taglib.html.TOKEN=" + token + "&txtDisciplina=9&txtTurma=a&cboSubG=" + SEMGRAD[0] + "&cboSubP=-1")
-
-    mech.select_form("FormSelecionarNivelPeriodoDisciplina")
-    mech["cboSubG"] = SEMGRAD
-    mech["cboSubP"] = SEMPOS_FORM
-    mech["cboAno"] = ANO
-    mech["txtDisciplina"]  = txtDisciplina
-    res1 = mech.submit()
+    res1 = mech.open("http://www.daconline.unicamp.br/altmatr/conspub_situacaovagaspordisciplina.do?org.apache.struts.taglib.html.TOKEN=" + token + "&txtDisciplina=" + disc + "&txtTurma=V&cboSubG=" + SEMGRAD + "&cboSubP="+ SEMPOS + "&cboAno=" + ANO + "&btnAcao=Continuar")
 
 # parseia o código da página e retira uma lista com as turmas
     site = res1.read()
@@ -111,56 +130,40 @@ def get_matriculados(txtDisciplina):
     turma = re.findall(dturma, site)
    
     for t in turma:
-        res2 = mech.open("http://www.daconline.unicamp.br/altmatr/conspub_matriculadospordisciplinaturma.do?org.apache.struts.taglib.html.TOKEN=" + token + "&txtDisciplina=9&txtTurma=a&cboSubG=" + SEMGRAD[0] + "&cboSubP=-1")
-
-        mech.select_form("FormSelecionarNivelPeriodoDisciplina")
-        mech["cboSubG"] = SEMGRAD
-        mech["cboSubP"] = SEMPOS_FORM
-        mech["cboAno"] = ANO
-        mech["txtDisciplina"] = txtDisciplina
-        mech["txtTurma"] = t
-        res3 = mech.submit()
+        res2 = mech.open("http://www.daconline.unicamp.br/altmatr/conspub_matriculadospordisciplinaturma.do?org.apache.struts.taglib.html.TOKEN=" + token + "&txtDisciplina=" + disc  + "&txtTurma=" + t + "&cboSubG=" + SEMGRAD + "&cboSubP=" + SEMPOS + "&cboAno=" + ANO + "&btnAcao=Continuar")
 
         # verifica se a turma tem alunos, isto é,
         # se está disponivel o link DRE_FILE no site.
-        s_file = res3.read()
+        s_file = res2.read()
         d_file = re.compile(DRE_FILE)
         m = re.search(d_file, s_file)
         if m is None:
             continue
 
-        print "Processando %s%s" % (txtDisciplina, t)
+        print "Processando %s%s" % (disc, t)
 
         # salva o arquivo com os dados da turma,
         # faz um iconv e abre de novo
         j = mech.open("https://www.daconline.unicamp.br/altmatr/fileDownloadPublico.do")
-        f = open(txtDisciplina + t + ".ascii", 'w')
-        f.write(j.read()) 
-        f.close()
-        os.system("iconv -f iso8859-1 -t utf-8 " + txtDisciplina + t + ".ascii >" + \
-                txtDisciplina + t + ".utf8")
-        f = open(txtDisciplina + t + ".utf8", 'r')
-        s_turma = f.read()
-        f.close()
-        os.remove(txtDisciplina + t + ".utf8")
-        os.remove(txtDisciplina + t + ".ascii")
+        s_turma = iconv_file(j.read(), disc + t)
 
         dprof = re.compile(DRE_PROF)
         m = re.search(dprof, s_turma)
         
-
         # inclui o docente no BD
         s = Professor.objects.filter(nome=m.group('docente')) 
         if len(s) == 0:
             Professor(nome=m.group('docente')).save()
         
         p = Professor.objects.filter(nome=m.group('docente'))
-        d = Disciplina.objects.filter(sigla=txtDisciplina)
+        d = Disciplina.objects.filter(sigla=disc)
 
         dalu = re.compile(DRE_ALUNO)
         alunos = re.findall(dalu, s_turma)
+        # cria a atribuicao
         at = Atribuicao(disciplina = d[0], professor = p[0], turma=t, semestre='2008-08-01')
         at.save()
+        #inclui os alunos
         for i in alunos:
             al = Aluno(RA=i[0], nome= i[1], curso= i[2])
             al.save()
@@ -172,29 +175,35 @@ def get_matriculados(txtDisciplina):
 # Por enquanto temos:
 
 INSTITUTO='IC'
-ANO=['2008']
+ANO='2008'
+SEMESTRE='2'
 
-SEMGRAD = ['2']
-SEMPOS_FORM = ['0']
+
+## GRAD #
+SEMGRAD = SEMESTRE
+SEMPOS = '0'
 NIVEL = 'G'
 BASE_SITE = "http://www.dac.unicamp.br/sistemas/horarios/grad/G" \
-    + SEMGRAD[0] + "S0/"
+    + SEMESTRE + "S0/"
 
 ld = get_disc_grad()
 for d in ld:
     get_matriculados(d)
-  
-SEMGRAD = ['0']
-SEMPOS_URL = '2'
-SEMPOS_FORM = ['22']
+## fim GRAD #
+
+
+## POS #
+SEMGRAD = '0'
+SEMPOS = SEMESTRE
+SEMPOS = '2' + SEMESTRE
 NIVEL =  'P'
 BASE_SITE = "wget http://www.dac.unicamp.br/sistemas/horarios/pos/P" \
-    + SEMPOS_URL + "S/"
-
+    + SEMESTRE + "S/"
 
 ld = get_disc_pos()
 for d in ld:
     get_matriculados(d)
+## fim POS #   
 
 print "Done."
 
