4ef251cd19b15d75aaa39dcfb5c02ad43d22fffd
Gustavo F. Padovan <gustavo@las.ic.unicamp.br>
2008-11-03 01:17:56 +0000

    add turma on regex add logout and auth urls
    --gut
    
    
    
    git-svn-id: https://sad-fenix.googlecode.com/svn/trunk@44 aa523ed0-0652-0410-bfd5-2b4be68e55b5

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import login, logout
 from caco.sad import views
 
 # Uncomment the next two lines to enable the admin:
@@ -14,13 +15,18 @@ urlpatterns = patterns('',
                            # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        
                        # Uncomment the next line to enable the admin:
-                           (r'^admin/(.*)', admin.site.root),
+                       (r'^admin/(.*)', admin.site.root),
+
+					   (r'^$', views.home),	   
+					   (r'^auth/$', views.login_auth),	   
+					   (r'^logout/$', views.logout),	   
                        (r'^[Ii][Cc]/$', views.show_all_semesters),
                        (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/$', views.show_all_courses),
-                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)/$', views.show_all_answers),
+                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)(?P<turma>[A-Z1-9#])/$', views.show_all_answers),
                        (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/answer/$', views.all_to_answer),
-                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)/answer/$', views.answer_course),
-                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)/commit/$', views.commit_answer_course),
+                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)(?P<turma>[A-Z1-9#])/answer/$', views.answer_course),
+                       (r'^[Ii][Cc]/(?P<ano>\d+)[sS](?P<semestre>\d)/(?P<disciplina>[A-Z][A-Z]\d+)(?P<turma>[A-Z1-9#])/commit/$', views.commit_answer_course),
                        (r'^(?P<path>.*\.css)$', 'django.views.static.serve', {'document_root': 'templates/css/' }),
                        (r'^(?P<path>.*\.(jpg|png|gif))$', 'django.views.static.serve', {'document_root': 'templates/img/' }),
                        )
+
